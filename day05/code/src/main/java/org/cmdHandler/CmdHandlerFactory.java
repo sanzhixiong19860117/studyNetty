dfb852894eff3d4d46224b246ed4b3b1664b4c7e
joy <315570835@qq.com>
2020-05-09 16:14:34 +0800

    怎么加了对去文件的操作还有读取文件的工具类

--- day05/code/src/main/java/org/cmdHandler/CmdHandlerFactory.java
+++ day05/code/src/main/java/org/cmdHandler/CmdHandlerFactory.java
@@ -1,10 +1,16 @@
 package org.cmdHandler;
 
 import com.google.protobuf.GeneratedMessageV3;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tinygame.herostory.msg.GameMsgProtocol;
+import org.util.PackageUtil;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author joy
@@ -13,6 +19,9 @@ import java.util.Map;
  * 指令静态工厂类
  */
 public final class CmdHandlerFactory {
+
+    static private final Logger LOGGER = LoggerFactory.getLogger(CmdHandlerFactory.class);
+
     /**
      * 处理器字典
      */
@@ -22,8 +31,67 @@ public final class CmdHandlerFactory {
     }
 
     static public void init() {
-        _handlerMap.put(GameMsgProtocol.UserEntryCmd.class, new UserEntryCmdHandler());
-        _handlerMap.put(GameMsgProtocol.WhoElseIsHereCmd.class, new WhoElseIsHereCmdHandler());
+//        _handlerMap.put(GameMsgProtocol.UserEntryCmd.class, new UserEntryCmdHandler());
+//        _handlerMap.put(GameMsgProtocol.WhoElseIsHereCmd.class, new WhoElseIsHereCmdHandler());
+        //获取包名称
+        final String packageName = CmdHandlerFactory.class.getPackage().getName();
+
+        //获得搜有deicmadhandler子类
+        Set<Class<?>> clazzSet = PackageUtil.listSubClazz(
+                packageName,
+                true,
+                ICmdHandler.class
+        );
+
+        for (Class<?> clazz : clazzSet) {
+            if ((clazz.getModifiers() & Modifier.ABSTRACT) != 0) {
+                //判断是否是抽象类
+                continue;
+            }
+
+            //获取方法数组
+            Method[] methods = clazz.getDeclaredMethods();
+
+            //循环方法
+            Class<?> msgType = null;
+
+            for (Method currMethod : methods) {
+                if (!currMethod.getName().equals("handle")) {
+                    //如果不是handle就不需要执行
+                    continue;
+                }
+
+                //获取方法的参数
+                Class<?>[] paramTypeArray = currMethod.getParameterTypes();
+
+                //判断参数是不是小于2，小于2肯定不是,判断第二个参数是不是我们对应的v3的里面的对象
+                if (paramTypeArray.length < 2 ||
+                        paramTypeArray[1] == GeneratedMessageV3.class ||
+                        !GeneratedMessageV3.class.isAssignableFrom(paramTypeArray[1])) {
+
+                    continue;
+                }
+
+                if (null == msgType) {
+                    continue;
+                }
+
+                try {
+                    ICmdHandler<?> newHandler = (ICmdHandler<?>) clazz.newInstance();
+                    //打印数据
+                    LOGGER.info(
+                            "关联{}<==>{}",
+                            msgType.getName(),
+                            clazz.getName()
+                    );
+                    _handlerMap.put(msgType, newHandler);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
--- day05/code/src/main/java/org/cmdHandler/UserMoveToCmdHandler.java
+++ day05/code/src/main/java/org/cmdHandler/UserMoveToCmdHandler.java
@@ -1,6 +1,8 @@
 package org.cmdHandler;
 
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tinygame.herostory.msg.GameMsgProtocol;
 
 /**
@@ -10,9 +12,18 @@ import org.tinygame.herostory.msg.GameMsgProtocol;
  * 移动操作
  */
 public class UserMoveToCmdHandler implements ICmdHandler<GameMsgProtocol.UserMoveToCmd> {
+    static private Logger LOGGER = LoggerFactory.getLogger(UserMoveToCmdHandler.class);
+
     @Override
     public void handle(ChannelHandlerContext ctx, GameMsgProtocol.UserMoveToCmd userMoveToCmd) {
         //第一步判断数据是否为null
+        if(null == ctx
+        || null == userMoveToCmd){
+            LOGGER.error("传入的移动数据不正确");
+            return;
+        }
+
+
         
     }
 }
